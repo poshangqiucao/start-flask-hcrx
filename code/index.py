80dc210886703a8b06baf93fc78d9820aa706daa
poshang <2890122341@qq.com>
2022-04-04 20:39:14 +0800

    设置模板

--- code/index.py
+++ code/index.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__,
+            static_url_path='/static', # 配置静态文件的访问 url 前缀
+            static_folder='static',    # 配置静态文件的文件夹
+            template_folder='templates')
 
 @app.route('/')
 def index():
-    return "hello world!"
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
